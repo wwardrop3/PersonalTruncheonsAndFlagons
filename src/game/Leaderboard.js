692d306083d6be5d9b53ec2389a6eeea24e092fe
Wil Wardrop <wwardrop@gmail.com>
2022-02-24 10:59:34 -0600

    added davids table formatting for later

--- src/game/Leaderboard.js
+++ src/game/Leaderboard.js
@@ -33,3 +33,65 @@ export const LeaderBoard = () => {
     return html
 
 }
+
+
+
+   
+// //import function from scoreList
+// import { TeamRows } from "./score/ScoreList.js";
+
+// // function for leaderboard
+// export const Leaderboard = () => {
+//     //interpolate html by invoking scoreList function
+//     //return html
+//     // set up table properly for scroll ability
+//     return `
+// <h2>Leaderboard</h2>
+// <table id="leaderboardTable">
+//     <thead>
+//         <tr class="leaderboardHeaders">
+//             <th scope="col" class="leaderboardHeader">Name</th>
+//             <th scope="col" class="leaderboardHeaderPlayer">Players</th>
+//             <th scope="col" class="leaderboardHeaderScore">Score</th>
+//         </tr>
+//     </thead>
+//     <tbody>
+//         <tr>
+//             <td colspan="3">
+//                 <div class="teamRows">
+//                     <table>
+//                         <tbody>
+//                             ${TeamRows()}
+//                         </tbody>
+//                     </table>
+//                 </div>
+//             </td>
+//         </tr>
+//     </tbody>
+// </table>
+// `
+// }
+
+// export const TeamRows = () => {
+//     const teams = getTeams()
+//     //make a copy of teams array and save their totalScore as a property
+//     const totalTeamScores = teams.map((team) => ({
+//         ...team,
+//         totalScore: totalScore(team.id)
+//     }))
+
+//     //sort all teams by their totalScore property so the leaderboard is displayed in the right order
+//     const sortedTeams = totalTeamScores.sort((team1, team2) => team2.totalScore - team1.totalScore)
+//     let html = ""
+//     for (const team of sortedTeams) {
+//         //interpolate string for each team that displays team name, totalScore, and playerCount
+//         html += `<tr class="leaderboardRows">
+//                     <td class="leaderboardTeam">${team.teamName}</td>
+//                     <td class="playerCount">${PlayerCount(team.id)}</td>
+//                     <td class="score">${totalScore(team.id)}</td>
+//                 </tr>`
+//     }
+    
+//     //return html
+//     return html
+// }
